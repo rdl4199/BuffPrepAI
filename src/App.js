1d74f046f874899614ae9df06bbfa2c2748e823b
Ebun O <remioludemi@gmail.com>
2023-07-06 14:45:53 -0400

    added main image

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './NavBar';
+import kidsLearningimg from "./kids learning.jpeg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <NavBar />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="Home--mainImg">
+          <img alt="" src={kidsLearningimg}/>
+        </div>
       </header>
     </div>
   );
