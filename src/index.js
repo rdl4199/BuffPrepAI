028830cf3d1b4b71ae44b7ec19e96c189f8ac034
rdl4199 <69980967+rdl4199@users.noreply.github.com>
2023-07-06 15:34:28 -0400

    Styling html and css more
    .

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App/App';
+import Learning from './Learning/Learning'
 import reportWebVitals from './reportWebVitals';
+import NavBar from './NavBar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ 
+  <Router>
+    <Switch>
+      <Route path="/" exact>
+        <NavBar />
+      </Route>
+      <Route path="/Learning" exact>
+        <Learning />
+      </Route>
+      <Route path="/App">
+        <App />
+      </Route>
+    </Switch>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
