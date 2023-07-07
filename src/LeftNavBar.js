81ca5839b6a40fc7447a699f46e459fc19acfde0
Ebun O <remioludemi@gmail.com>
2023-07-07 10:58:30 -0400

    added icons

--- src/LeftNavBar.js
+++ src/LeftNavBar.js
@@ -7,12 +7,12 @@ const LeftNavBar = () => {
     <leftnav>
       <div className='LeftNav-linksContainer'>
         <ul className="LeftNav--links">
-              <li><a href="https://buffaloprep.com/about/"><i className='fas fa-user'></i>Profile</a></li>
-              <li><a href="https://buffaloprep.com/admissions/"><i className='fas fa-search'></i>AI Search</a></li>
-              <li><a href="https://buffaloprep.com/admissions/"><i className='fas fa-book'></i>AI Study</a></li>
-              <li><a><Link to="/Learning"><i className='fas fa-tools'></i>AI Tools</Link></a></li>
-              <li><a href="https://buffaloprep.com/admissions/"><i className='fas fa-graduation-cap'></i>Courses</a></li>
-              <li><a href="https://buffaloprep.com/admissions/"><i className='fas fa-chart-bar'></i>Grades</a></li>
+              <li><a href="https://buffaloprep.com/about/"><i className='fas fa-user'></i> Profile</a></li>
+              <li><a href="https://buffaloprep.com/admissions/"><i className='fas fa-search'></i> AI Search</a></li>
+              <li><a href="https://buffaloprep.com/admissions/"><i className='fas fa-book'></i> AI Study</a></li>
+              <li><a><Link to="/Learning"><i className='fas fa-tools'></i> AI Tools</Link></a></li>
+              <li><a href="https://buffaloprep.com/admissions/"><i className='fas fa-graduation-cap'></i> Courses</a></li>
+              <li><a href="https://buffaloprep.com/admissions/"><i className='fas fa-chart-bar'></i> Grades</a></li>
         </ul>
       </div>
     </leftnav>
